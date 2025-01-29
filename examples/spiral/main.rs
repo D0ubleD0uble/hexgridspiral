ac188e740801a0fd60be6aed038e0db53a79bee0
LucidBrot <eric@mink.li>
2025-01-29 18:51:16 +0100

    Add an example

---
+++ examples/spiral/main.rs
@@ -0,0 +1,27 @@
+use hexgridspiral::HGSTile;
+fn main() {
+    // Make a HexGridSpiral Tile at index 3
+    let a = HGSTile::make(3);
+    // Compute its CubeCoordinates
+    let b = a.cc();
+    println!(
+        "The Index {} has the tile center at cube-coordinate {}",
+        a, b
+    );
+
+    // Let's spawn twenty hex-tiles around the origin
+    let step_size = 1.;
+    for i in 0..21 {
+        let tile = HGSTile::make(i).cc().to_pixel((0., 0.), step_size.into());
+        // Let's say you want to add tiles to your levelmap in the game UI:
+        add_tile_to_UI(tile, i);
+    }
+}
+
+fn add_tile_to_UI(position: (f64, f64), i: u64) {
+    // I don't want to add a huge dependency to this example, so imagine a GUI.
+    println!(
+        "The GUI would position tile {} at pixel-position ({},{})",
+        i, position.0, position.1
+    );
+}
